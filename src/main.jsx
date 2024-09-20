114ff34bfc00c87a85ed7d2e29ed4ddeaf2ca572
Theinterna <yalvin58@gmail.com>
2024-09-20 11:20:32 -0700

    Add styling to the website to make it look better

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import './index.css'
 
 function NavBar() {
   return (
-    <nav>
-      <img src="./react-logo.png" width = "100px" />
-      <ul className='navbar'>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
+    <nav className="flex justify-between items-center p-4 shadow-md">
+      <img src="./react-logo.png" className='w-14' />
+      <ul className = "flex list-none">
+        <li className='p-2.5'>Pricing</li>
+        <li className='p-2.5'>About</li>
+        <li className='p-2.5'>Contact</li>
       </ul>
     </nav>
   )
@@ -17,27 +17,32 @@ function NavBar() {
 
 function Footer() {
   return (
-    <footer>
-      <p style={{
-        fontSize: "10px"
-      }}>Copyright 2024 @yalvin58 :P</p>
+    <footer className='flex mt-auto'>
+      <p className='text-lg text-white bg-gray-500 w-screen p-3'>Copyright 2024 @yalvin58 :P</p>
     </footer>
   )
 }
 
-function Page() {
-  return(
-    <>
-      <NavBar />
-      <h1>Fun Facts About React</h1>
-      <ul>
+function Body() {
+  return (
+    <div className='flex flex-col p-4'>
+      <h1 className='text-3xl pb-3 pt-1 font-serif font-bold'>Fun Facts About React</h1>
+      <ul className = "list-disc pl-7">
         <li>It was released in 2013</li>
         <li>First created by Jordan Walke</li>
         <li>Well over 100K stars on Github</li>
         <li>Is maintained by Facebook!</li>
       </ul>
+    </div>
+  )
+}
+function Page() {
+  return(
+    <div className='flex flex-col h-screen'>
+      <NavBar />
+      <Body />
       <Footer />
-    </>
+    </div>
   )
 }
 createRoot(document.getElementById('root')).render(
