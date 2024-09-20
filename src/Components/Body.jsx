6b7fac871695181915f679b539f36c8aaddcc897
Theinterna <yalvin58@gmail.com>
2024-09-20 13:12:35 -0700

    Change body colors and background color

---
+++ src/Components/Body.jsx
@@ -0,0 +1,15 @@
+function Body() {
+    return (
+        <div className='flex flex-col p-4'>
+        <h1 className='text-3xl pb-3 pt-1 font-serif font-bold text-white'>Fun Facts About React</h1>
+        <ul className = "list-disc pl-7 marker:text-light-blue text-white">
+            <li>It was released in 2013</li>
+            <li>First created by Jordan Walke</li>
+            <li>Well over 100K stars on Github</li>
+            <li>Is maintained by Facebook!</li>
+        </ul>
+        </div>
+    )
+}
+
+export default Body;
--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './Components/Header';
+import Body from './Components/Body';
+import Footer from './Components/Footer';
 
-function NavBar() {
-  return (
-    <nav className="flex justify-between items-center p-4 shadow-md">
-      <img src="./react-logo.png" className='w-14' />
-      <ul className = "flex list-none">
-        <li className='p-2.5'>Pricing</li>
-        <li className='p-2.5'>About</li>
-        <li className='p-2.5'>Contact</li>
-      </ul>
-    </nav>
-  )
-}
-
-function Footer() {
-  return (
-    <footer className='flex mt-auto'>
-      <p className='text-lg text-white bg-gray-500 w-screen p-3'>Copyright 2024 @yalvin58 :P</p>
-    </footer>
-  )
-}
-
-function Body() {
-  return (
-    <div className='flex flex-col p-4'>
-      <h1 className='text-3xl pb-3 pt-1 font-serif font-bold'>Fun Facts About React</h1>
-      <ul className = "list-disc pl-7">
-        <li>It was released in 2013</li>
-        <li>First created by Jordan Walke</li>
-        <li>Well over 100K stars on Github</li>
-        <li>Is maintained by Facebook!</li>
-      </ul>
-    </div>
-  )
-}
-function Page() {
+function App() {
   return(
-    <div className='flex flex-col h-screen'>
-      <NavBar />
+    <div className='flex flex-col h-screen bg-mid-gray'>
+      <Header />
       <Body />
       <Footer />
     </div>
@@ -47,6 +16,6 @@ function Page() {
 }
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Page />
+    <App />
   </StrictMode>,
 )
