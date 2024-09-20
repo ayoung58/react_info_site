dba5ad15db78d9ea53f7cd056ebc925557b8fd0e
Theinterna <yalvin58@gmail.com>
2024-09-20 13:33:36 -0700

    Change bullet point size

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,7 +2,7 @@ function Body() {
     return (
         <div className='flex flex-col p-4'>
         <h1 className='text-3xl pb-3 pt-1 font-inter font-bold text-white'>Fun Facts About React</h1>
-        <ul className = "list-disc pl-7 marker:text-light-blue text-white font-sofadi">
+        <ul className = "list-disc pl-7 marker:text-light-blue marker:text-2xl text-white font-sofadi">
             <li>It was released in 2013</li>
             <li>First created by Jordan Walke</li>
             <li>Well over 100K stars on Github</li>
