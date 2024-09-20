b25defce548ac8c56c7989b972620afad1233962
Theinterna <yalvin58@gmail.com>
2024-09-20 13:08:00 -0700

    Add new color themes for palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'light-blue': "#61DAFB",
+        'dark-gray': "#21222A",
+        'mid-gray': "#282D35"
+      },
+    },
   },
   plugins: [],
 }
