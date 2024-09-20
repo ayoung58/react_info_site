d6bbff08b73e38a750e467961b0f856b78bea745
Theinterna <yalvin58@gmail.com>
2024-09-19 21:50:30 -0700

    Add components for the page and for nav and footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+function NavBar() {
+  return (
+    <nav>
+      <img src="./react-logo.png" width = "100px" />
+    </nav>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p style={{
+        fontSize: "10px"
+      }}>Copyright 2024 @yalvin58 :P</p>
+    </footer>
+  )
+}
+
+function Page() {
+  return(
+    <>
+      <NavBar />
+      <h1>Fun Facts About React</h1>
+      <ul>
+        <li>It was released in 2013</li>
+        <li>First created by Jordan Walke</li>
+        <li>Well over 100K stars on Github</li>
+        <li>Is maintained by Facebook!</li>
+      </ul>
+      <Footer />
+    </>
+  )
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <img src="./react-logo.png" width = "100px" />
-    <h1>Fun Facts About React</h1>
-    <ul>
-      <li>It was released in 2013</li>
-      <li>First created by Jordan Walke</li>
-      <li>Well over 100K stars on Github</li>
-      <li>Is maintained by Facebook!</li>
-    </ul>
+    <Page />
   </StrictMode>,
 )
